e489d2e81e2aea8a3b88049f62dd815129aaaadb
Jerome White <jerome@thewex.net>
2020-01-07 16:47:22 +0530

    Use multiprocessing queue's instead of pool's
    Makes error handling and return values more straightforward.

--- card2disk.py
+++ card2disk.py
@@ -3,26 +3,13 @@ import sys
 import csv
 import stat
 import shutil
+from string import Template
 from pathlib import Path
 from datetime import datetime
 from argparse import ArgumentParser
-from multiprocessing import Pool
+from multiprocessing import Pool, JoinableQueue
 
-class Result:
-    def __init__(self, source, target=None):
-        self.source = source
-        self.target = target
-
-    def __bool__(self):
-        return self.target is not None
-
-    def __str__(self):
-        return self.source.stem + ' -> ' + str(self.target) if self else ''
-
-def func(args):
-    (exif, destination) = args
-    source = Path(exif['SourceFile'])
-
+def exif2path(exif, suffix):
     #
     # Extract the time at which the picture was taken
     #
@@ -31,51 +18,78 @@ def func(args):
         'DateTimeOriginal',
         'ModifyDate',
     )
+
     for i in filter(lambda x: x in exif, keys):
         creation = exif[i]
         break
     else:
-        return Result(source)
-
-    destfmt = [ '%Y', '%m-%b', '%d-%H%M%S' ]
-    ctime = datetime.strptime(str(creation), '%Y:%m:%d %H:%M:%S')
-    (*relative, base) = [ ctime.strftime(x).upper() for x in destfmt ]
+        raise ValueError('Cannot establish creation time')
 
     #
-    # Use the time to create a destination file name
+    # Use the time to build a filename
     #
-    path = destination.joinpath(*relative)
-    suffix = source.suffix.lower()
-    for i in range(100):
-        fname = '{0}-{1:02d}'.format(base, i)
-        target = path.joinpath(fname).with_suffix(suffix)
+    ctime = (datetime
+             .strptime(creation, '%Y:%m:%d %H:%M:%S')
+             .strftime('%Y/%m-%b/%d-%H%M%S'))
+
+    return Path(ctime.upper()).with_suffix(suffix)
+
+#
+# Use the time to create a destination file name
+#
+def path2fname(path, destination, maxtries):
+    tstring = '{}-$version{}'.format(path.parent.joinpath(path.stem),
+                                     path.suffix.lower())
+    template = Template(tstring)
+
+    for i in range(maxtries):
+        version = '{:02d}'.format(i)
+        fname = template.substitute(version=version)
+        target = destination.joinpath(fname)
         if not target.exists():
-            break
-    else:
-        return Result(source)
+            return target
 
-    (src, dst) = map(str, (source, target))
+    raise ValueError('Cannot create unique filename')
 
-    #
-    # Disk access!
-    #
-    target.parent.mkdir(parents=True, exist_ok=True)
-    shutil.copy2(src, dst)
-    os.chmod(dst, stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH)
+def func(queue, destination, maxtries):
+    while True:
+        exif = queue.get()
+        source = Path(exif['SourceFile'])
 
-    return Result(source, target)
+        try:
+            path = exif2path(exif, source.suffix.lower())
+            target = path2fname(path, destination, maxtries)
+            (src, dst) = map(str, (source, target))
+
+            #
+            # Disk access!
+            #
+            target.parent.mkdir(parents=True, exist_ok=True)
+            shutil.copy2(src, dst)
+            os.chmod(dst, stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH)
+
+            print(source.stem, '->', target)
+        except ValueError as err:
+            print('Error:', source, err, file=sys.stderr)
+
+        queue.task_done()
 
 arguments = ArgumentParser()
 arguments.add_argument('--destination', type=Path)
+arguments.add_argument('--maxtries', type=int, default=100)
+arguments.add_argument('--workers', type=int)
 # arguments.add_argument('--adjust')
 args = arguments.parse_args()
 
-with Pool() as pool:
-    reader = csv.DictReader(sys.stdin)
-    iterable = map(lambda x: (x, args.destination), reader)
+queue = JoinableQueue()
+initargs = (
+    queue,
+    args.destination,
+    args.maxtries,
+)
 
-    for (i, result) in enumerate(pool.imap_unordered(func, iterable)):
-        if not result:
-            print('Error:', result.source, file=sys.stderr)
-        else:
-            print(i, result)
+with Pool(args.workers, func, initargs):
+    reader = csv.DictReader(sys.stdin)
+    for row in reader:
+        queue.put(row)
+    queue.join()
