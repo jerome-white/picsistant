7e98c66e65994c7e1c1b72c88ce9d62770c5dfdc
Jerome White <jerome@thewex.net>
2024-10-05 11:35:06 +0530

    Expose video archiving capability

--- card2disk.sh
+++ card2disk.sh
@@ -4,18 +4,20 @@ export PYTHONLOGLEVEL=info
 STUDIO=`mktemp --directory`
 
 source=`mktemp --directory --tmpdir=$STUDIO`
-while getopts 'a:d:s:h' option; do
+while getopts 'a:d:s:vh' option; do
     case $option in
 	a) adjust=$OPTARG ;;
         d) destination=$OPTARG ;;
         s)
 	    ln --symbolic "`realpath "$OPTARG"`" $source/`uuid -v4`
 	    ;;
+	v) videos="--with-videos" ;;
         h)
 	    cat <<EOF
 Usage: $0
  -s Source
  -d Destination
+ -v Also archive video
  -a Adjust picture timestamp: [+|-]=[H:MM]
     This argument is passed directly to exiftool's -AllDates
     option; remember to quote! See the exiftool manpage.
@@ -37,6 +39,6 @@ fi
 
 caffeinate -i bash <<EOF
 exiftool -recurse -csv -quiet $source \
-    | python card2disk.py --destination $destination
+    | python card2disk.py $videos --destination $destination
 rm --recursive --force $STUDIO
 EOF
